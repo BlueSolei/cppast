6de3a5450fc8186bfd99d832a7061c372a21bffe
Shaul Fridman <shaul.fridman@gmail.com>
2019-12-17 23:37:57 +0200

    (+) conan build passes

--- conanfile.py
+++ conanfile.py
@@ -1,47 +1,46 @@
+# -*- coding: utf-8 -*-
+
 from conans import ConanFile, CMake, tools
 
 
 class CppastConan(ConanFile):
     name = "cppast"
     version = "0.1"
-    license = "<Put the package license here>"
-    author = "<Put your name here> <And your email here>"
-    url = "<Package recipe repository url here, for issues about the package>"
-    description = "<Description of Cppast here>"
-    topics = ("<Put some tag here>", "<here>", "<and here>")
+    license = "MIT License"
+    author = "Jonathan Müller <git@foonathan.net>"
+    url = "https://github.com/BlueSolei/cppast"
+    description = "Library to parse and work with the C++ AST"
+    topics = ("cplusplus", "ast", "parser-library", "libclang")
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
     default_options = {"shared": False}
     generators = "cmake"
 
     def source(self):
-        self.run("git clone https://github.com/conan-io/hello.git")
+        repo = tools.Git()
+        repo.clone(self.url, shallow=True)
+        
         # This small hack might be useful to guarantee proper /MT /MD linkage
         # in MSVC if the packaged project doesn't have variables to set it
         # properly
-        tools.replace_in_file("hello/CMakeLists.txt", "PROJECT(HelloWorld)",
-                              '''PROJECT(HelloWorld)
+        tools.replace_in_file("CMakeLists.txt", "project(cppast VERSION 0.0)",
+                              '''project(cppast VERSION 0.0)
 include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
 conan_basic_setup()''')
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_folder="hello")
+        cmake.configure()
         cmake.build()
 
-        # Explicit way:
-        # self.run('cmake %s/hello %s'
-        #          % (self.source_folder, cmake.command_line))
-        # self.run("cmake --build . %s" % cmake.build_config)
-
     def package(self):
-        self.copy("*.h", dst="include", src="hello")
-        self.copy("*hello.lib", dst="lib", keep_path=False)
+        self.copy("*.h", dst="include", src="cppast/include")
+        self.copy("*cppast.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.so", dst="lib", keep_path=False)
         self.copy("*.dylib", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = ["hello"]
+        self.cpp_info.libs = ["cppast"]
 
